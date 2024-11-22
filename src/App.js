ac995c92ec019fb0d375ac2c0bc414fe0d3ef234
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-23 00:03:57 +0530

    modify main screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,30 +6,35 @@ import Contacts from './pages/Contacts';
 import Company from './pages/Company';
 import Product from './pages/Product';
 import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="navbar">
         <Link to="/">Home</Link>
-        <br />
         <Link to="/about">About</Link>
-        <br />
         <Link to="/contacts">Contacts</Link>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/about/company" element={<Company />} />
-
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/product/:productId " element={<Product />} />
-
-        <Route path="*" element={<NotFound />} />
-
-      </Routes>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/about/company" element={<Company />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <footer>
+        © {new Date().getFullYear()} My React App. Built with ❤️. 
+        <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+          Learn More
+        </a>
+      </footer>
     </BrowserRouter>
   );
 }
+
 
 export default App;
