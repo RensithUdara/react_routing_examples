b44d800540c374576a651b1f39f9079b5c379026
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-23 00:04:43 +0530

    modify product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
 
-function Product () {
-    const params = useParams();
+function Product() {
+  const { productId } = useParams();
 
-    console.log(params);
-
-    return <div>Product</div>
+  return (
+    <div className="page">
+      <h1>Product Details</h1>
+      <p>You are viewing details for product ID: {productId}</p>
+    </div>
+  );
 }
 
 export default Product;
